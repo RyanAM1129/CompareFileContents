34747d07f8a8a287c93319d6a06c2c5737e803d2
RyanAM1129 <RyanAMacLeod@hotmail.com>
2022-04-11 19:57:51 -0700

    commit two

---
+++ src/controller/Verifier.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class Verifier {
+    public static void checkFileExists(final String theFileName) {
+
+    }
+}
--- src/view/Display.java
+++ src/view/Display.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Display {
@@ -9,23 +12,51 @@ public class Display {
      * False when you want to stop, true when you want to continue.
      */
     private static boolean myFlag;
+    /**
+     * Keeps count of the current line.
+     */
+    private static int myLineCount;
 
-    public void mainMenu() {
+    public static void mainMenu() {
         Scanner theConsole = new Scanner(System.in);
-        String theFirstFileName, theSecondFileName;
+        String theFirstFileName = null, theSecondFileName = null;
         myFlag = false;
         while(!myFlag) {
             System.out.println("Please enter the first filepath: ");
             theFirstFileName = theConsole.nextLine();
             //Write if-statement to check if filename exists.
+            myFlag = true;
         }
         myFlag = false;
         while(!myFlag) {
             System.out.println("Please enter the second filepath: ");
             theSecondFileName = theConsole.nextLine();
             //Write if-statement to check if filename exists.
+            myFlag = true;
         }
 
         //Call method that checks files.
+        BufferedReader theFirstReader, theSecondReader;
+        try {
+            theFirstReader = new BufferedReader(new FileReader(theFirstFileName));
+            theSecondReader = new BufferedReader(new FileReader(theSecondFileName));
+            String theFirstLine = theFirstReader.readLine();
+            String theSecondLine = theSecondReader.readLine();
+            myLineCount = 1;
+            while (theFirstLine != null) {
+                if(!theFirstLine.equals(theSecondLine)) {
+                    System.out.println("Not Equal at line " + myLineCount);
+                    System.out.println(theFirstLine);
+                    System.out.println(theSecondLine);
+                }
+                // read next line
+                theFirstLine = theFirstReader.readLine();
+                theSecondLine = theSecondReader.readLine();
+                myLineCount++;
+            }
+            theFirstReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
